f0d18766ed5dfbada3e3be331829681aaabd1955
marcelbuesing <buesing.marcel@googlemail.com>
2015-12-19 12:36:50 +0100

    Add input labels

--- src/Lib.hs
+++ src/Lib.hs
@@ -24,14 +24,32 @@ data LoginFields = LoginFields {
   , _rememberMe :: Bool
   } deriving (Show)
 
+mNotEmpty :: (Eq a, Monoid a, MonadPlus m) => m a -> m a
+mNotEmpty = mfilter (not . (== mempty))
+
 metaViewport :: MonadWidget t m => String -> m ()
 metaViewport s = elAttr "meta" ("name" =: "viewport" <> "content" =: s) blank
 
 stylesheet :: MonadWidget t m => String -> m ()
-stylesheet s = elAttr "link" (Map.fromList [("rel", "stylesheet"), ("href", s)]) $ return ()
+stylesheet s = elAttr "link" (Map.fromList [("rel", "stylesheet"), ("href", s)]) blank
 
 scriptSrc :: MonadWidget t m => String -> m ()
-scriptSrc s = elAttr "script" (Map.fromList [("type", "javascript"), ("src", s)]) $ return ()
+scriptSrc s = elAttr "script" (Map.fromList [("src", s)]) blank
+
+label :: MonadWidget t m => String -> String -> m ()
+label l f = labelClass l f ""
+
+labelClass :: MonadWidget t m => String -> String -> String -> m ()
+labelClass l f c = elAttr "label" ("for" =: f <> "class" =: c) $ text l
+
+-- | s - button text, c - button class
+buttonClass :: MonadWidget t m => String -> String -> m (Event t ())
+buttonClass s c = do
+  (e, _) <- elAttr' "button" ("class" =: c) $ text s
+  return $ domEvent Click e
+
+formClass :: MonadWidget t m => String -> m ()
+formClass c = elAttr "form" ("class" =: c) blank
 
 headSection :: Widget Spider (Gui Spider (WithWebView SpiderHost) SpiderHostFrame) ()
 headSection = do
@@ -46,58 +64,40 @@ someFunc = mainWidgetWithHead headSection login
 row :: forall t m a. MonadWidget t m => m a -> m a
 row = divClass "row"
 
-mNotEmpty :: (Eq a, Monoid a, MonadPlus m) => m a -> m a
-mNotEmpty = mfilter (not . (== mempty))
+rowClass :: forall t m a. MonadWidget t m => String ->m a -> m a
+rowClass c= divClass $ "row " <> c
 
 login :: MonadWidget t m => m ()
 login = divClass "center" $
-  elClass "form" "login" $ do
+  elAttr "form" ("action" =: "#" <> "class" =: "login") $ do
     elClass "h3" "Login" $ text "Login"
     z <- loginField
     x <- mapDyn show z
     row $ do
-      elAttr "a" ("href" =: "/register") $ text "Register"
-      elAttr "a" ("href" =: "#") $ text "Forgot password?"
+      elAttr "a" ("href" =: "/register" <> "class" =: "frm-btn mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect") $ text "Register"
+      elAttr "a" ("href" =: "#" <> "class" =: "frm-btn mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect") $ text "Forgot password?"
+    row $ buttonClass "Login" "frm-btn mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored"
     dynText x
 
 loginField :: MonadWidget t m => m (Dynamic t (Maybe LoginFields))
 loginField = do
   lc <- loginCredentials
-  -- el "label" $ text "Remember me"
-  cb <- row $ checkbox False def
+  cb <- rowClass "mdl-textfield" $ do
+    labelClass "" "remember" "mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect"
+    checkbox False $ def & attributes .~ constDyn ("id" =: "remember" <> "class" =: "mdl-checkbox__input")
   combineDyn (\x y -> LoginFields <$> x <*> Just y) lc (_checkbox_value cb)
 
 loginCredentials :: MonadWidget t m => m (Dynamic t (Maybe UserFields))
 loginCredentials = do
-  user <- row $ textInput def
-  pw <- row $ textInput def
+  user <- rowClass "mdl-textfield mdl-js-textfield mdl-textfield--floating-label" $ do
+    ti <- textInput $ def & attributes .~ constDyn ("id" =: "email" <> "name" =: "email" <> "type" =: "email" <> "class" =: "mdl-textfield__input")
+    labelClass "E-Mail" "email" "mdl-textfield__label"
+    return $ ti
+  pw <- rowClass "mdl-textfield mdl-js-textfield mdl-textfield--floating-label" $ do
+    ti <- textInput $ def & attributes .~ constDyn ("id" =: "pass" <> "name" =: "pass" <> "type" =: "password" <> "class" =: "mdl-textfield__input")
+    labelClass "Password" "pass" "mdl-textfield__label"
+    return $ ti
   mayUser <- mapDyn (mNotEmpty . Just) $ _textInput_value user
   mayPw <- mapDyn (mNotEmpty . Just) $ _textInput_value pw
   combineDyn (\x y -> UserFields <$> x <*> y) mayUser mayPw
 
-   --el "div" $ do
-  ----  elAttr "div" ("class" =: "center")
-  ----  return ()
-
---    form_ [class_ "login", method_ "post", action_ "/login"] $ do
---      h3_ "Login"
---     el "div" $ [class_ "row"] $
---        input_ [id_ "email", name_ "email",
---                placeholder_ "E-Mail",
---                type_ "text"]
---     el "div" $ [class_ "row"] $
---        input_ [id_ "pass", name_ "pass",
---                placeholder_ "Password",
---                type_ "password"]
---     el "div" $ [class_ "row"] $ do
---        label_ [for_ "remember"] "Remember me"
---        input_ [id_ "remember",
---                type_ "checkbox",
---                name_ "remember",
---                value_ "remember"]
---     el "div" $ [class_ "row"] $ do
---        a_ [href_ "/register"] "Register"
---        a_ [href_ "#"] "Forgot password?"
---     el "div" $ [class_ "row"] $
---        button_ [type_ "submit",
---                 value_ "submit"] "Login"
